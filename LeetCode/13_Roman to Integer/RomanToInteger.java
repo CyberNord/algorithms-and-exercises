d4f1061c5b82bb0bfe7e6fd09a2a4ee0db7d5e22
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2025-10-08 19:09:58 +0200

    RomanToInteger

---
+++ LeetCode/13_Roman to Integer/RomanToInteger.java
@@ -0,0 +1,34 @@
+package integers.int_romanToIntegers;
+
+//13. Roman to Integer
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int length = s.length() - 1;
+        int i = 0;
+
+        while (i <= length) {
+            if (i < length && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
+                result -= romanMap.get(s.charAt(i));
+            } else {
+                result += romanMap.get(s.charAt(i));
+            }
+            i++;
+        }
+        return result;
+    }
+}
